3807004722495e47f8f75bd2d77ae662fd76a80b
Istvan Fodor <programtervezo@gmail.com>
2016-11-01 08:51:19 +0100

    remove unused file

--- run.js
+++
@@ -1,7 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
-    if (tab.url.indexOf("9gag.com") > -1 &&
-        changeInfo.url === undefined){
-
-        chrome.tabs.executeScript(tabId, {file: "program.js"} );
-    }
-});
